34d734de13c6ca2705090774ee039dca59f669d0
Marko Davidovic <davidovic-m@hotmail.com>
2020-06-10 22:29:09 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
-  state = { quote:'', }
+  state = { quote:'',
+            author: '',}
 
   componentDidMount(){
     // console.log("componentDidMount")
@@ -16,9 +17,11 @@ class App extends React.Component {
      axios.get("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
         .then((response) => {
           const quote = response.data[this.randomNumber(0, response.data.length-1)].quote;
+          const author = response.data[this.randomNumber(0, response.data.length-1)].author;
 
           this.setState({ quote });
-           // console.log(response.data[this.randomNumber(0, response.data.length-1)].quote);
+          this.setState({ author });
+           // console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
@@ -30,8 +33,9 @@ class App extends React.Component {
 
     return(
       <div className="app">
-        <div className="card">
+        <div className="card col-md-10">
           <h1 className="heading">{this.state.quote}</h1>
+          <h2>"<i>{this.state.author}</i>"</h2>
           <button className="button" onClick={this.fetchQuote}>
             <span>GIVE ME QUOTE!</span>
           </button>
